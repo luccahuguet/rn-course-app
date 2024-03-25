aa4f9666d16651b0b06ef507d0d1cfecf18b49af
luccahuguet <lucca.huguet2@gmail.com>
2024-03-25 19:59:41 -0300

    v1

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput placeholder="Enter a goal" />
+        <Button title='Submit' onPress={() => console.log('Button pressed')} />
+      </View>
+      <View>
+        <Text> List of Goals </Text>
+      </View>
     </View>
   );
 }
